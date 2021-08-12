9716aebd268978063580b83f685ac63324084faf
Matheus Eidt <matheus@mx2.unisc.br>
2021-08-12 19:49:15 -0300

    Created tests to Revendedor model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
-from django.test import TestCase
+from core import models
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
+from django.test import TestCase
 
 
 class ModelTests(TestCase):
@@ -11,17 +12,14 @@ class ModelTests(TestCase):
         email = 'teste@grupoboticario.com.br'
         password = 'pass123'
         name = 'User 1'
-        cpf = '763.416.260-45'
         user = get_user_model().objects.create_user(
             email=email,
             password=password,
-            name=name,
-            cpf=cpf
+            name=name
         )
 
         self.assertEqual(user.email, email)
         self.assertEqual(user.name, name)
-        self.assertEqual(user.cpf, cpf)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -30,8 +28,7 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(
             email=email,
             password='pass123',
-            name='user 1',
-            cpf='458.803.680-76'
+            name='user 1'
         )
 
         self.assertEqual(user.email, email.lower())
@@ -42,8 +39,7 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user(
                 email=None,
                 password='pass123',
-                name='name',
-                cpf='713.765.400-29'
+                name='name'
             )
 
     def test_create_new_superuser(self):
@@ -51,8 +47,7 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(
             email='teste@grupoboticario.com.br',
             password='pass123',
-            name='super user 1',
-            cpf='495.592.240-69'
+            name='super user 1'
         )
 
         self.assertTrue(user.is_superuser)
@@ -65,34 +60,70 @@ class ModelTests(TestCase):
         get_user_model().objects.create_user(
             email=email,
             password='pass123',
-            name='user 1',
-            cpf='458.803.680-76'
+            name='user 1'
         )
 
         with self.assertRaises(IntegrityError):
             get_user_model().objects.create_user(
                 email=email,
                 password='123pass',
-                name='user 2',
-                cpf='446.389.490-53'
+                name='user 2'
             )
+    # endregion
 
-    def test_create_users_with_same_cpf_fails(self):
-        """Test creating two users with same cpf raises error"""
-        cpf = '446.389.490-53'
+    # region Revendedor Tests
+    def test_create_revendedor_successful(self):
+        """Test creating revendedor successful"""
+        email = 'teste@grupoboticario.com.br'
+        password = 'pass123'
+        name = 'User 1'
+        cpf = '713.765.400-29'
+        user = models.Revendedor.objects.create_user(
+            email=email,
+            password=password,
+            name=name,
+            cpf=cpf
+        )
 
-        get_user_model().objects.create_user(
-            email='user@grupoboticario.com.br',
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.name, name)
+        self.assertTrue(user.check_password(password))
+        self.assertEqual(user.cpf, cpf)
+
+    def test_create_revendedor_same_cpf_fails(self):
+        """Test creating revendedor with same cpf raises error"""
+        cpf = '713.765.400-29'
+        models.Revendedor.objects.create_user(
+            email='user1@grupoboticario.com.br',
             password='pass123',
-            name='user 1',
+            name='revendedor 1',
             cpf=cpf
         )
 
         with self.assertRaises(IntegrityError):
-            get_user_model().objects.create_user(
+            models.Revendedor.objects.create_user(
                 email='user2@grupoboticario.com.br',
                 password='123pass',
-                name='user 2',
+                name='revendedor 2',
                 cpf=cpf
             )
+
+    def test_create_revendedor_same_email_fails(self):
+        """Test creating revendedor with same email raises error"""
+        email = 'revendedor@grupoboticario.com.br'
+        models.Revendedor.objects.create_user(
+            email=email,
+            password='pass123',
+            name='revendedor 1',
+            cpf='761.563.350-80'
+        )
+
+        with self.assertRaises(IntegrityError):
+            models.Revendedor.objects.create_user(
+                email=email,
+                password='123pass',
+                name='revendedor 2',
+                cpf='093.118.300-62'
+            )
+
     # endregion
