3710e3183d26b3707cb70fad844813a258d36310
Matheus Eidt <matheus@mx2.unisc.br>
2021-08-14 17:52:23 -0300

    Creating views and serializers for cashback API.

--- app/core/models.py
+++ app/core/models.py
@@ -67,5 +67,35 @@ class Compra(models.Model):
     revendedor = models.ForeignKey(Revendedor, on_delete=models.CASCADE)
     status = models.IntegerField(choices=Status.choices, default=1)
 
+    @property
+    def month_total(self):
+        return sum([c.value for c in Compra.objects.filter(
+            revendedor=self.revendedor,
+            date__year=self.date.year,
+            date__month=self.date.month
+        )])
+
+    @property
+    def cashback_percent(self):
+        if self.month_total > 1500:
+            return 20
+        elif self.month_total > 1000:
+            return 15
+        else:
+            return 10
+
+    @property
+    def cashback_value(self):
+        return round(self.value * (self.cashback_percent / 100), 2)
+
+    @property
+    def status_str(self):
+        if self.status == 1:
+            return 'Em validação'
+        elif self.status == 2:
+            return 'Aprovado'
+        else:
+            return 'Não aprovado'
+
     def __str__(self) -> str:
         return str(self.code)
